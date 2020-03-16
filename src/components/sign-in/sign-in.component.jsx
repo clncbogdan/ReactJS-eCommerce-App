0241aeab8be716d959b99f4ca4a365fe744087f6
Bogdan <bogdan.george.calancea@gmail.com>
2020-03-16 16:20:52 +0200

    Create a custom form input to replace the default input tag

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FormInput from '../form-input/form-input.component';
 
 import './sign-in.styles.scss';
 
@@ -33,22 +34,22 @@ class SignIn extends React.Component {
 				<span>Sign in with your email and password</span>
 
 				<form onSubmit={this.handleSubmit}>
-					<input
+					<FormInput
 						name="email"
 						type="email"
 						value={this.state.email}
 						onChange={this.handleChange}
 						required
+						label="Email"
 					/>
-					<label>Email</label>
-					<input
+					<FormInput
 						name="password"
 						type="password"
 						value={this.state.password}
 						onChange={this.handleChange}
 						required
+						label="Password"
 					/>
-					<label>Password</label>
 					<input type="submit" value="SIGN IN" />
 				</form>
 			</div>
