fd1570dae036346a36c6acaed5dc38fa1842e00b
Bogdan <bogdan.george.calancea@gmail.com>
2020-03-16 16:38:32 +0200

    Replace default submit btn with custom button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
@@ -50,7 +51,7 @@ class SignIn extends React.Component {
 						required
 						label="Password"
 					/>
-					<input type="submit" value="SIGN IN" />
+					<CustomButton type="submit">SIGN IN</CustomButton>
 				</form>
 			</div>
 		);
