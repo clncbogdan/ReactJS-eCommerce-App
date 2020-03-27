0aecebad94d89f1f5b0168d07d787f577c76a713
Bogdan <bogdan.george.calancea@gmail.com>
2020-03-27 19:38:55 +0200

    Add sign in with email and password handler

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -16,13 +16,22 @@ class SignIn extends React.Component {
 		};
 	}
 
-	handleSubmit = event => {
+	handleSubmit = async event => {
 		event.preventDefault();
 
-		this.setState({
-			email: '',
-			password: '',
-		});
+		const { email, password } = this.state;
+
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({
+				email: '',
+				password: '',
+			});
+		} catch (error) {
+			console.error(
+				'There was an error when logging in. ' + error.message
+			);
+		}
 	};
 
 	handleChange = event => {
